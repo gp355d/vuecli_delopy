da419f06d8b1cd15da07c4f9bd81f670aa3b96b3
gp355d <chichali374@gmail.com>
2020-07-28 21:37:39 +0800

    移除loading元件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-          <loading :active.sync="isLoading"></loading>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
