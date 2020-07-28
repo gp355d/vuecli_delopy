037ee64320e0ffe38ba2b8d6dd6be3ea2b08c5d3
leo <chichali374@gmail.com>
2020-07-28 21:49:21 +0800

    加入loading元件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+       <loading :active.sync="isLoading"></loading>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/products">產品列表</router-link> |
