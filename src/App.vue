727941c03e08b0643d65ab7485fcff8654c17a59
leo <chichali374@gmail.com>
2020-07-28 22:41:24 +0800

    增加分隔線

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/products">產品列表</router-link> |
-       <router-link to="/cart">購物車</router-link>
+       <router-link to="/cart">購物車</router-link> |
        <router-link to="/admin/products">後臺產品列表</router-link>
     </div>
     123
